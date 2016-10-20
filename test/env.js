727f5eb3caff2630382ec52e02cf1b9d0e271cf5
coderaiser <coderaiser@cloudcmd.io>
2016-10-20 10:26:53 +0300

    fix(env) $npm_package_config: name with "_"

--- test/env.js
+++ test/env.js
@@ -38,3 +38,18 @@ test('env: $npm_package_config', (t) => {
     t.end();
 });
 
+test('env: $npm_package_config: name with "_"', (t) => {
+    const result = {
+        npm_package_config_compile_client_min: 'hello'
+    };
+    
+    const config = env.config({
+        compile: {
+            client_min: 'hello'
+        }
+    });
+    
+    t.deepEqual(config, result, 'should build npm config');
+    t.end();
+});
+
